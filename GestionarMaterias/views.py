c3bfe015ec9420afd13f12cb2474547133e55fa3
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-08-22 19:29:41 -0500

    Se agrego Bootstrap en el frontend

--- GestionarMaterias/views.py
+++ GestionarMaterias/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import MaricularMaterias
 
 # Create your views here.
+def home(request):
+    materias = MaricularMaterias.objects.all()
+    contexto = {"materias": materias}
+    return render(request, "home.html", context=contexto)
