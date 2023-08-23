37c6d9852b6cd2a5a304c261230b7cba094b8082
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-08-22 20:47:05 -0500

    se modifico la base de datos

--- GestionarMaterias/models.py
+++ GestionarMaterias/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class MaricularMaterias(models.Model):
     id = models.CharField(primary_key=True,  max_length=5)
-    nombre = models.CharField(max_length=17, blank=False, null=False)
+    nombre = models.CharField(max_length=40, blank=False, null=False)
     creditos = models.PositiveSmallIntegerField(blank=False, null=False)
     fecha = models.DateTimeField(auto_now=True)
 
