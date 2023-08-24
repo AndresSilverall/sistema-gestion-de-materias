0b6caea2218e0c00f64e98f009965701f51e1822
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-08-23 20:09:51 -0500

    se configuro el formulario de matricular materias

--- GestionarMaterias/views.py
+++ GestionarMaterias/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from . models import MaricularMaterias
 
 # Create your views here.
 def home(request):
-    materias = MaricularMaterias.objects.all()
+    materias = MaricularMaterias.objects.all().order_by("nombre")
     contexto = {"materias": materias}
     return render(request, "gestionMaterias.html", context=contexto)
 
 
 def register_topic(request):
     id = request.POST["codigo"]
-    nombe = request.POST["nombre"]
+    nombre = request.POST["nombre"]
     creditos = request.POST["creditos"]
     if request.method == "POST":
-        materias = MaricularMaterias.objects.create(id=id, nombre=nombe, creditos=creditos)
+        materias = MaricularMaterias.objects.create(id=id, nombre=nombre, creditos=creditos)
         materias.save()
-        redirect("register")
+        messages.success(request, '¡Materia matriculada con exito!')
+
+        return redirect("register")
+    
     return render(request, "gestionMaterias.html")
 
 
